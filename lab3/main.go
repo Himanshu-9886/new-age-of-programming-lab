6ed5506984e7274fb5922b11aeb3806d2b11284b
Himanshu-9886 <007himanshukaushik@gmail.com>
2026-09-15 10:44:16 +0530

    Update main.go

--- lab3/main.go
+++ lab3/main.go
@@ -5,36 +5,34 @@ import "fmt"
 type Person struct {
 	Name   string
 	Age    int
-	Job string
-	Salary float32
+	Job    string
+	Salary float64
 }
 
-// function by pointer
-func (s *Person) readData() {
-	fmt.Print("Enter person name: ")
-	fmt.Scanln(&s.Name)
-	fmt.Print("Enter person age: ")
-	fmt.Scanln(&s.Age)
-	fmt.Print("Enter person job: ")
-	fmt.Scanln(&s.Job)
-	fmt.Print("Enter person salary: ")
-	fmt.Scanln(&s.Salary)
+func (p Person) readData() Person {
+	fmt.Print("Enter name: ")
+	fmt.Scan(&p.Name)
+	fmt.Print("Enter age: ")
+	fmt.Scan(&p.Age)
+	fmt.Print("Enter job: ")
+	fmt.Scan(&p.Job)
+	fmt.Print("Enter salary: ")
+	fmt.Scan(&p.Salary)
+	return p
 }
 
-//method
-func (s Person) printData() {
-	fmt.Print("Name: ", s.Name)
-	fmt.Print("\nAge: ", s.Age)
-	fmt.Print("\nJob: ", s.Job)
-	fmt.Print("\nSalary: ", s.Salary)
+func (p Person) PrintData() {
+	fmt.Print("Name: ", p.Name)
+	fmt.Print("\nAge: ", p.Age)
+	fmt.Print("\nJob: ", p.Job)
+	fmt.Print("\nSalary: ", p.Salary)
 }
+
 func main() {
-	var person1 Person
-	var person2 Person	
-	fmt.Println("\nthe Details of first person")
-	person1.readData()
-	person1.printData()
-	fmt.Println("the Details of second person")
-	person2.readData()
-	person2.printData()
+	var p2 Person
+	var p1 Person
+	p1 = p1.readData()
+	p2 = p2.readData()
+	p1.PrintData()
+	p2.PrintData()
 }
